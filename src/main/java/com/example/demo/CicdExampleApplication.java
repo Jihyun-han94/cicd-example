95f614547681b55799518c5b71de816e386fdce2
JihyunHan <gkswlgus923@gamil.com>
2024-12-21 11:22:47 +0900

    server port 삭제, contextpath 설정

--- src/main/java/com/example/demo/CicdExampleApplication.java
+++ src/main/java/com/example/demo/CicdExampleApplication.java
@@ -2,10 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class CicdExampleApplication {
-
+	
+	@GetMapping("/")
+	public String helloWorld() {
+		return "helloWorld";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CicdExampleApplication.class, args);
 	}
